f7c72f8492d67f32ec88cd422db8221127e4d352
Jannis Leidel <jezdez@enn.io>
2008-11-21 12:49:17 +0000

    Updated setup.py, INSTALL notice and MANIFEST template

---
+++ messages/__init__.py
@@ -0,0 +1,2 @@
+VERSION = (0, 3, 1)
+__version__ = '.'.join(map(str, VERSION))
--- setup.py
+++ setup.py
@@ -1,41 +1,24 @@
 from distutils.core import setup
-import os
 
-# Compile the list of packages available, because distutils doesn't have
-# an easy way to do this.
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir:
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk('messages'):
-    # Ignore dirnames that start with '.'
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        pkg = dirpath.replace(os.path.sep, '.')
-        if os.path.altsep:
-            pkg = pkg.replace(os.path.altsep, '.')
-        packages.append(pkg)
-    elif filenames:
-        prefix = dirpath[13:] # Strip "messages/" or "messages\"
-        for f in filenames:
-            data_files.append(os.path.join(prefix, f))
-
-setup(name='messages',
-      version='0.3',
-      description='User-to-user messaging system for Django',
-      author='Arne Brodowski',
-      author_email='mail@arnebrodowski.de',
-      url='http://code.google.com/p/django-messages/',
-      packages=packages,
-      package_dir={'messages': 'messages'},
-      package_data={'messages': data_files},
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='django-messages',
+    version=__import__('messages').__version__,
+    description='User-to-user messaging system for Django',
+    author='Arne Brodowski',
+    author_email='mail@arnebrodowski.de',
+    url='http://code.google.com/p/django-messages/',
+    packages=(
+        'messages',
+        'messages.templatetags',
+    ),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Framework :: Django',
+    ),
+)
