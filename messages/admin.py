aae3c0dbdc474b1b031c35ac2085ac53920f72e0
Jannis Leidel <jezdez@enn.io>
2009-01-19 16:26:47 +0000

    Fixes issue 17 - adding mass messaging feature in the admin

--- messages/admin.py
+++ messages/admin.py
@@ -1,4 +1,90 @@
+import itertools
 from django.contrib import admin
+from django.utils.itercompat import is_iterable
+
+from django import forms
+from django.contrib import admin
+from django.db.models import get_app
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ImproperlyConfigured
+from django.contrib.auth.models import User, Group
+
 from messages.models import Message 
 
-admin.site.register(Message)
+class MessageAdminForm(forms.ModelForm):
+    """
+    Custom AdminForm to enable messages to groups and all users.
+    """
+    recipient = forms.ModelChoiceField(
+        label=_('Recipient'), queryset=User.objects.all(), required=True)
+
+    group = forms.ChoiceField(label=_('group'), required=False,
+        help_text=_('Creates the message optionally for all users or a group of users.'))
+
+    def __init__(self, *args, **kwargs):
+        super(MessageAdminForm, self).__init__(*args, **kwargs)
+        self.fields['group'].choices = self._get_group_choices()
+
+    def _get_group_choices(self):
+        return [('', u'---------'), ('all', _('All users'))] + \
+            [(group.pk, group.name) for group in Group.objects.all()]
+
+    class Meta:
+        model = Message
+
+class MessageAdmin(admin.ModelAdmin):
+    form = MessageAdminForm
+    fieldsets = (
+        (None, {
+            'fields': (
+                'sender',
+                ('recipient', 'group'),
+            ),
+        }),
+        (_('Message'), {
+            'fields': (
+                'parent_msg',
+                'subject', 'body',
+            ),
+            'classes': ('monospace' ),
+        }),
+        (_('Date/time'), {
+            'fields': (
+                'sent_at', 'read_at', 'replied_at',
+                'sender_deleted_at', 'recipient_deleted_at',
+            ),
+            'classes': ('collapse', 'wide'),
+        }),
+    )
+    list_display = ('subject', 'sender', 'recipient', 'sent_at', 'read_at')
+    list_filter = ('sent_at', 'sender', 'recipient')
+    search_fields = ('subject', 'body')
+
+    def save_model(self, request, obj, form, change):
+        """
+        Saves the message for the recipient and looks in the form instance
+        for other possible recipients. Prevents duplication by excludin the
+        original recipient from the list of optional recipients.
+
+        When changing an existing message and choosing optional recipients,
+        the message is effectively resent to those users.
+        """
+        obj.save()
+        if form.cleaned_data['group'] == 'all':
+            # send to all users
+            recipients = User.objects.exclude(pk=obj.recipient.pk)
+        else:
+            # send to a group of users
+            recipients = []
+            group = form.cleaned_data['group']
+            if group:
+                group = Group.objects.get(pk=group)
+                recipients.extend(
+                    list(group.user_set.exclude(pk=obj.recipient.pk)))
+        # create messages for all found recipients
+        for user in recipients:
+            obj.pk = None
+            obj.recipient = user
+            obj.save()
+
+admin.site.register(Message, MessageAdmin)
