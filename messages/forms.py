d5c4430f522e000ad2ced6dddc4bbb1947cf7eb5
Arne Brodowski <arne@rcs4u.de>
2010-01-07 16:08:20 +0000

    Fixed a bug where notifications where send multiple times if a message has multiple recipients and django-notification is used. Fixes Issue #47.

--- messages/forms.py
+++ messages/forms.py
@@ -51,8 +51,8 @@ class ComposeForm(forms.Form):
             if notification:
                 if parent_msg is not None:
                     notification.send([sender], "messages_replied", {'message': msg,})
-                    notification.send(recipients, "messages_reply_received", {'message': msg,})
+                    notification.send([r], "messages_reply_received", {'message': msg,})
                 else:
                     notification.send([sender], "messages_sent", {'message': msg,})
-                    notification.send(recipients, "messages_received", {'message': msg,})
+                    notification.send([r], "messages_received", {'message': msg,})
         return message_list
