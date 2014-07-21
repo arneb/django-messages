659a3dd710051f54e3edc1d76cdfb910d7d04c1a
Arne Brodowski <mail@arnebrodowski.de>
2014-07-21 16:22:18 +0200

    python 3 compatibility and whitespce cleanup. fixes #30

--- django_messages/templatetags/inbox.py
+++ django_messages/templatetags/inbox.py
@@ -3,7 +3,7 @@ from django.template import Library, Node, TemplateSyntaxError
 class InboxOutput(Node):
     def __init__(self, varname=None):
         self.varname = varname
-        
+
     def render(self, context):
         try:
             user = context['user']
@@ -14,32 +14,32 @@ class InboxOutput(Node):
             context[self.varname] = count
             return ""
         else:
-            return "%s" % (count)        
-        
+            return "%s" % (count)
+
 def do_print_inbox_count(parser, token):
     """
     A templatetag to show the unread-count for a logged in user.
     Returns the number of unread messages in the user's inbox.
     Usage::
-    
+
         {% load inbox %}
         {% inbox_count %}
-    
+
         {# or assign the value to a variable: #}
-        
+
         {% inbox_count as my_var %}
         {{ my_var }}
-        
+
     """
     bits = token.contents.split()
     if len(bits) > 1:
         if len(bits) != 3:
             raise TemplateSyntaxError("inbox_count tag takes either no arguments or exactly two arguments")
         if bits[1] != 'as':
-            raise TemplateSyntaxError, "first argument to inbox_count tag must be 'as'"
+            raise TemplateSyntaxError("first argument to inbox_count tag must be 'as'")
         return InboxOutput(bits[2])
     else:
         return InboxOutput()
 
-register = Library()     
+register = Library()
 register.tag('inbox_count', do_print_inbox_count)
