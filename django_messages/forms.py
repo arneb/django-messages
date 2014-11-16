710c76324c9ca4eef4be1b3082cc567ed7c95cc5
Chris Mitchell <chris.mit7@gmail.com>
2014-11-16 17:42:30 -0500

    option to disable notification

--- django_messages/forms.py
+++ django_messages/forms.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
-if "notification" in settings.INSTALLED_APPS:
+if "notification" in settings.INSTALLED_APPS and getattr(settings, 'DJANGO_MESSAGES_NOTIFY', True):
     from notification import models as notification
 else:
     notification = None
