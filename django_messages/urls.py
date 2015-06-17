c23570f528b3ad27e256ea402fc02231b528b000
v1ct0r-LH <v1ct0r-LH@users.noreply.github.com>
2015-06-17 17:22:31 +0000

    Set an explicit value because Default value of 'RedirectView.permanent' will change from True to False in Django 1.9.

--- django_messages/urls.py
+++ django_messages/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import RedirectView
 from django_messages.views import *
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='inbox/'), name='messages_redirect'),
+    url(r'^$', RedirectView.as_view(permanent=True, url='inbox/'), name='messages_redirect'),
     url(r'^inbox/$', inbox, name='messages_inbox'),
     url(r'^outbox/$', outbox, name='messages_outbox'),
     url(r'^compose/$', compose, name='messages_compose'),
