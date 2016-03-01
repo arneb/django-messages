e461c10c75f9c49c473a26c3e54629e8068d5e92
Tobias Goecke <tobias.goecke@chemtics.com>
2016-03-01 22:34:49 +0100

    adaptations for chemtics
    Signed-off-by: Tobias Goecke <tobias.goecke@chemtics.com>

--- django_messages/views.py
+++ django_messages/views.py
@@ -12,12 +12,15 @@ from django_messages.models import Message
 from django_messages.forms import ComposeForm
 from django_messages.utils import format_quote, get_user_model, get_username_field
 
+from profiles.models import ProfileProfileConnection
+
 User = get_user_model()
 
 if "notification" in settings.INSTALLED_APPS and getattr(settings, 'DJANGO_MESSAGES_NOTIFY', True):
     from notification import models as notification
 else:
     notification = None
+
 
 @login_required
 def inbox(request, template_name='django_messages/inbox.html'):
@@ -26,10 +29,13 @@ def inbox(request, template_name='django_messages/inbox.html'):
     Optional Arguments:
         ``template_name``: name of the template to use.
     """
+    page_menu_id = 1
     message_list = Message.objects.inbox_for(request.user)
     return render_to_response(template_name, {
         'message_list': message_list,
+        'page_menu_id': page_menu_id,
     }, context_instance=RequestContext(request))
+
 
 @login_required
 def outbox(request, template_name='django_messages/outbox.html'):
@@ -38,10 +44,14 @@ def outbox(request, template_name='django_messages/outbox.html'):
     Optional arguments:
         ``template_name``: name of the template to use.
     """
+
+    page_menu_id = 2
     message_list = Message.objects.outbox_for(request.user)
     return render_to_response(template_name, {
         'message_list': message_list,
+        'page_menu_id': page_menu_id,
     }, context_instance=RequestContext(request))
+
 
 @login_required
 def trash(request, template_name='django_messages/trash.html'):
@@ -52,14 +62,17 @@ def trash(request, template_name='django_messages/trash.html'):
     Hint: A Cron-Job could periodicly clean up old messages, which are deleted
     by sender and recipient.
     """
+    page_menu_id = 3
     message_list = Message.objects.trash_for(request.user)
     return render_to_response(template_name, {
         'message_list': message_list,
+        'page_menu_id': page_menu_id,
     }, context_instance=RequestContext(request))
+
 
 @login_required
 def compose(request, recipient=None, form_class=ComposeForm,
-        template_name='django_messages/compose.html', success_url=None, recipient_filter=None):
+            template_name='django_messages/compose.html', success_url=None, recipient_filter=None):
     """
     Displays and handles the ``form_class`` form to compose new messages.
     Required Arguments: None
@@ -71,6 +84,23 @@ def compose(request, recipient=None, form_class=ComposeForm,
         ``template_name``: the template to use
         ``success_url``: where to redirect after successfull submission
     """
+    profile_list = []
+    profiles_one = ProfileProfileConnection.objects.filter(sec_profile__username=request.user.username,
+                                                           rejected_connection=False,
+                                                           connected=True)
+    for profile in profiles_one:
+        profile_user = ProfileProfileConnection.objects.get(pk=str(profile)).sec_profile.username
+        variable = User.objects.get(username=profile_user)
+        profile_list.append(variable)
+
+    profiles_two = ProfileProfileConnection.objects.filter(first_profile__username=request.user.username,
+                                                           rejected_connection=False,
+                                                           connected=True)
+    for profile in profiles_two:
+        profile_user = ProfileProfileConnection.objects.get(pk=str(profile)).first_profile.username
+        variable = User.objects.get(username=profile_user)
+        profile_list.append(variable)
+
     if request.method == "POST":
         sender = request.user
         form = form_class(request.POST, recipient_filter=recipient_filter)
@@ -85,17 +115,20 @@ def compose(request, recipient=None, form_class=ComposeForm,
     else:
         form = form_class()
         if recipient is not None:
-            recipients = [u for u in User.objects.filter(**{'%s__in' % get_username_field(): [r.strip() for r in recipient.split('+')]})]
+            recipients = [u for u in User.objects.filter(
+                **{'%s__in' % get_username_field(): [r.strip() for r in recipient.split('+')]})]
             form.fields['recipient'].initial = recipients
     return render_to_response(template_name, {
         'form': form,
+        'profile_list':profile_list,
     }, context_instance=RequestContext(request))
+
 
 @login_required
 def reply(request, message_id, form_class=ComposeForm,
-        template_name='django_messages/compose.html', success_url=None,
-        recipient_filter=None, quote_helper=format_quote,
-        subject_template=_(u"Re: %(subject)s"),):
+          template_name='django_messages/compose.html', success_url=None,
+          recipient_filter=None, quote_helper=format_quote,
+          subject_template=_(u"Re: %(subject)s"), ):
     """
     Prepares the ``form_class`` form for writing a reply to a given message
     (specified via ``message_id``). Uses the ``format_quote`` helper from
@@ -121,11 +154,12 @@ def reply(request, message_id, form_class=ComposeForm,
         form = form_class(initial={
             'body': quote_helper(parent.sender, parent.body),
             'subject': subject_template % {'subject': parent.subject},
-            'recipient': [parent.sender,]
-            })
+            'recipient': [parent.sender, ]
+        })
     return render_to_response(template_name, {
         'form': form,
     }, context_instance=RequestContext(request))
+
 
 @login_required
 def delete(request, message_id, success_url=None):
@@ -162,6 +196,7 @@ def delete(request, message_id, success_url=None):
         return HttpResponseRedirect(success_url)
     raise Http404
 
+
 @login_required
 def undelete(request, message_id, success_url=None):
     """
@@ -189,10 +224,11 @@ def undelete(request, message_id, success_url=None):
         return HttpResponseRedirect(success_url)
     raise Http404
 
+
 @login_required
 def view(request, message_id, form_class=ComposeForm, quote_helper=format_quote,
-        subject_template=_(u"Re: %(subject)s"),
-        template_name='django_messages/view.html'):
+         subject_template=_(u"Re: %(subject)s"),
+         template_name='django_messages/view.html'):
     """
     Shows a single message.``message_id`` argument is required.
     The user is only allowed to see the message, if he is either
@@ -203,8 +239,10 @@ def view(request, message_id, form_class=ComposeForm, quote_helper=format_quote,
     If the user is the recipient a reply form will be added to the
     tenplate context, otherwise 'reply_form' will be None.
     """
+    message_list = Message.objects.inbox_for(request.user)
     user = request.user
     now = timezone.now()
+    mid = int(message_id)
     message = get_object_or_404(Message, id=message_id)
     if (message.sender != user) and (message.recipient != user):
         raise Http404
@@ -212,13 +250,13 @@ def view(request, message_id, form_class=ComposeForm, quote_helper=format_quote,
         message.read_at = now
         message.save()
 
-    context = {'message': message, 'reply_form': None}
+    context = {'message': message, 'mid': mid, 'message_list': message_list, 'reply_form': None}
     if message.recipient == user:
         form = form_class(initial={
             'body': quote_helper(message.sender, message.body),
             'subject': subject_template % {'subject': message.subject},
-            'recipient': [message.sender,]
-            })
+            'recipient': [message.sender, ]
+        })
         context['reply_form'] = form
     return render_to_response(template_name, context,
-        context_instance=RequestContext(request))
+                              context_instance=RequestContext(request))
