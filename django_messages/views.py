fffd9415a8ea4905f1583b6347b9aec531e4418c
tobiasgoecke <tobias.goecke@chemtics.com>
2016-03-10 00:05:52 +0100

    &ouml;
    Signed-off-by: tobiasgoecke <tobias.goecke@chemtics.com>

--- django_messages/views.py
+++ django_messages/views.py
@@ -198,7 +198,7 @@ def delete(request, message_id, success_url=None):
         message.save()
         messages.info(request, _(u"Message successfully deleted."))
         UserOnBoardNotification.objects.create(user=user, title="Nachricht", notify_typ="info",
-                                               notify_message=u"Nachricht gelöscht!")
+                                               notify_message="Nachricht gel&ouml;scht!")
         if notification:
             notification.send([user], "messages_deleted", {'message': message,})
         return HttpResponseRedirect(success_url)
