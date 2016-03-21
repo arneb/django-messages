61af4cbcb600b44c2c5d830298ea54096fec62e8
Darjan Salaj <salaj.au@gmail.com>
2016-03-21 10:40:48 +0100

    forward recipent on compose

--- django_messages/views.py
+++ django_messages/views.py
@@ -93,6 +93,7 @@ def compose(request, recipient=None, form_class=ComposeForm,
             form.fields['recipient'].initial = recipients
     return render_to_response(template_name, {
         'form': form,
+        'recipent': recipient
     }, context_instance=RequestContext(request))
 
 @login_required
