3e2d334ae923a6adbaa2bf24dcb98681c86f84e3
Chris Lawlor <lawlor.chris@gmail.com>
2016-11-18 16:08:50 -0500

    Updates test suite and README for Django 1.10

--- tests/settings.py
+++ tests/settings.py
@@ -32,3 +32,19 @@ DATABASES = {
         'NAME': os.path.join(os.path.dirname(__file__), 'database.db'),
     }
 }
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
