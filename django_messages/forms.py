b6917dc23d26d8ad2f5e97619663f8d8c2fd8ad2
Tobias Göcke <tobias.goecke@supratix.com>
2017-09-24 11:59:17 +0200

    bugfix
    Signed-off-by: Tobias Göcke <tobias.goecke@supratix.com>

--- django_messages/forms.py
+++ django_messages/forms.py
@@ -12,6 +12,7 @@ from django_messages.models import Message
 from django_messages.fields import CommaSeparatedUserField
 from profiles.models import UserOnBoardNotification
 
+
 class ComposeForm(forms.Form):
     """
     A simple default form for private messages.
@@ -19,15 +20,13 @@ class ComposeForm(forms.Form):
     recipient = CommaSeparatedUserField(label=_(u"Recipient"))
     subject = forms.CharField(label=_(u"Subject"), max_length=140)
     body = forms.CharField(label=_(u"Body"),
-        widget=forms.Textarea(attrs={'rows': '12', 'cols':'55'}))
-
+                           widget=forms.Textarea(attrs={'rows': '12', 'cols': '55'}))
 
     def __init__(self, *args, **kwargs):
         recipient_filter = kwargs.pop('recipient_filter', None)
         super(ComposeForm, self).__init__(*args, **kwargs)
         if recipient_filter is not None:
             self.fields['recipient']._recipient_filter = recipient_filter
-
 
     def save(self, sender, parent_msg=None):
         recipients = self.cleaned_data['recipient']
@@ -36,10 +35,10 @@ class ComposeForm(forms.Form):
         message_list = []
         for r in recipients:
             msg = Message(
-                sender = sender,
-                recipient = r,
-                subject = subject,
-                body = body,
+                sender=sender,
+                recipient=r,
+                subject=subject,
+                body=body,
             )
             if parent_msg is not None:
                 msg.parent_msg = parent_msg
@@ -47,14 +46,14 @@ class ComposeForm(forms.Form):
                 parent_msg.save()
             msg.save()
             message_list.append(msg)
+            UserOnBoardNotification.objects.create(user=recipient, title="Nachricht", notify_typ="info",
+                                                   notify_message="Hallo, " + str(
+                                                       sender) + " hat Ihnen eine Nachricht zugesendet!")
             if notification:
                 if parent_msg is not None:
-                    notification.send([sender], "messages_replied", {'message': msg,})
-                    notification.send([r], "messages_reply_received", {'message': msg,})
+                    notification.send([sender], "messages_replied", {'message': msg, })
+                    notification.send([r], "messages_reply_received", {'message': msg, })
                 else:
-                    notification.send([sender], "messages_sent", {'message': msg,})
-                    notification.send([r], "messages_received", {'message': msg,})
+                    notification.send([sender], "messages_sent", {'message': msg, })
+                    notification.send([r], "messages_received", {'message': msg, })
         return message_list
-            UserOnBoardNotification.objects.create(user=recipient, title="Nachricht", notify_typ="info",
-                                                   notify_message="Hi, " + str(
-                                                       sender) + " hat dir eine Nachricht zugesendet!")
