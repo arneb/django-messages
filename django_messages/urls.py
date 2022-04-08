172bdbfae8fc22aee6e25403ac89f78bf8d7463e
Andy Woods <andytwoods@users.noreply.github.com>
2022-04-08 08:55:27 +0100

    Update urls.py

--- django_messages/urls.py
+++ django_messages/urls.py
@@ -1,17 +1,16 @@
-from django.conf.urls import url
 from django.views.generic import RedirectView
-
+from django.urls import include, path
 from django_messages.views import *
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(permanent=True, url='inbox/'), name='messages_redirect'),
-    url(r'^inbox/$', inbox, name='messages_inbox'),
-    url(r'^outbox/$', outbox, name='messages_outbox'),
-    url(r'^compose/$', compose, name='messages_compose'),
-    url(r'^compose/(?P<recipient>[\w.@+-]+)/$', compose, name='messages_compose_to'),
-    url(r'^reply/(?P<message_id>[\d]+)/$', reply, name='messages_reply'),
-    url(r'^view/(?P<message_id>[\d]+)/$', view, name='messages_detail'),
-    url(r'^delete/(?P<message_id>[\d]+)/$', delete, name='messages_delete'),
-    url(r'^undelete/(?P<message_id>[\d]+)/$', undelete, name='messages_undelete'),
-    url(r'^trash/$', trash, name='messages_trash'),
+    path('', RedirectView.as_view(permanent=True, url='inbox/'), name='messages_redirect'),
+    path('inbox/', inbox, name='messages_inbox'),
+    path('outbox/', outbox, name='messages_outbox'),
+    path('compose/', compose, name='messages_compose'),
+    path('compose/(?P<recipient>[\w.@+-]+)/', compose, name='messages_compose_to'),
+    path('reply/(?P<message_id>[\d]+)/', reply, name='messages_reply'),
+    path('view/(?P<message_id>[\d]+)/', view, name='messages_detail'),
+    path('delete/(?P<message_id>[\d]+)/', delete, name='messages_delete'),
+    path('undelete/(?P<message_id>[\d]+)/', undelete, name='messages_undelete'),
+    path('trash/$', trash, name='messages_trash'),
 ]
