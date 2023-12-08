1a9d7fd25553bb110b1be5a77494d51f9db2ae45
Nicolás Gómez <nicolasdanielgomez@gmail.com>
2023-12-08 19:31:46 +0000

    Django 4.2 support

--- django_messages/apps.py
+++ django_messages/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
+try:
+    from django.utils.translation import ugettext_lazy as _
+except:
+    from django.utils.translation import gettext_lazy as _
+
 
 class DjangoMessagesConfig(AppConfig):
     name = 'django_messages'
